c08b14bc8074a1b6e70a3e0d478b2afce458209d
Detroner <ruslan_bykau@epam.com>
2024-10-28 11:41:36 +0300

    Add 09

---
+++ 09/car_4.py
@@ -0,0 +1,35 @@
+class Car():
+  """Простая модель автомобиля."""
+  def __init__(self, make, model, year):
+    """Инициализирует	атрибуты	описания	автомобиля."""
+    self.make = make
+    self.model = model
+    self.year = year
+    self.odometer_reading = 0
+
+
+  def get_descriptive_name(self):
+    """Возвращает аккуратно отформатированное описание."""
+    long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+    return long_name.title()
+
+  def read_odometer(self):
+    """Выводит пробег машины в милях."""
+    print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+  def update_odometer(self, mileage):
+    """Устанавливает заданное значение на одометре."""
+    if mileage >= self.odometer_reading:
+      self.odometer_reading = mileage
+    else:
+      print("You can't roll back an odometer!")
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(20)
+my_new_car.read_odometer()
